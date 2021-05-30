7e9fc2e2b350040d8e88f2e45ecc4ad0ba72e448
Артем Белышев <artembelysev@MacBook-Pro-Artem.local>
2021-05-30 15:38:04 +0200

    added links2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ function Footer() {
       <div className='footer'>
   			<div className='wrap_footer_rosee'><img src={rosee} className='footer_logo'/></div>
   				<a href='https://docs.google.com/document/d/1d4pEXBZh_aXOMoJkhbtwu4klflWcsoBYGcFbIQUPdFg/edit' className='nav_footer' id='footer_offer'>Оферта</a>
-  				<NavLink to='#' className='nav_footer' id='footer_confidentiality' activeClassName='active_footer'>Политика конфиденциальности</NavLink>
-  				<NavLink to='#' className='nav_footer' id='footer_sentence' activeClassName='active_footer'>Пользовательское предложение</NavLink>
+  				<a href='https://docs.google.com/document/d/1IcFeUEIdDZ6cU6kjTjYtYONfeamZvNOqsNXBeH7hl2k/edit' className='nav_footer' id='footer_confidentiality'>Политика конфиденциальности</a>
+  				<a href='https://docs.google.com/document/d/1EiJUgz1JSZKRvfzWYJpWcdhlXHZHRAF6xbwm0qXSqLs/edit' className='nav_footer' id='footer_sentence'>Пользовательское предложение</a>
 				<p className='nav_footer' id='footer_phone'>8 902 247 13 61</p>
 				<p className='nav_footer' id='footer_email'>hi@rosee.ws</p>
 				<p className='nav_footer' id='footer_information'><p className="information_1">ИП Поветьев Кирилл Владимирович</p> <p className="information_2">ИНН 562903440350</p> <p className="information_3">ОГРН 320565800048044</p></p>
