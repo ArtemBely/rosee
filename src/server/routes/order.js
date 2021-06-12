9e07ba70ea4bd28ee3d7b49f3e4a00f4e512ccf6
Артем Белышев <artembelysev@MacBook-Pro-Artem.local>
2021-06-12 14:02:59 +0200

    createHeroku

--- src/server/routes/order.js
+++ src/server/routes/order.js
@@ -34,6 +34,46 @@ router.get(['/everyMonth', '/halfYear', 'everyYear', '/'], notLoggedIn, (req, re
                    <link rel="stylesheet" type="text/css" href="../main.css">
                      <meta name="viewport" content="width=device-width, initial-scale=1">
                        <script src='/bundle.js' defer></script>
+
+                                                           <script>
+                                                           !function(f,b,e,v,n,t,s)
+                                                           {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                                                           n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                                                           if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                                                           n.queue=[];t=b.createElement(e);t.async=!0;
+                                                           t.src=v;s=b.getElementsByTagName(e)[0];
+                                                           s.parentNode.insertBefore(t,s)}(window, document,'script',
+                                                           'https://connect.facebook.net/en_US/fbevents.js');
+                                                           fbq('init', '299294384104664');
+                                                           fbq('track', 'PageView');
+                                                           </script>
+                                                           <noscript><img height="1" width="1" style="display:none"
+                                                           src="https://www.facebook.com/tr?id=299294384104664&ev=PageView&noscript=1"
+                                                           /></noscript>
+
+                                                           <script type="text/javascript" >
+                                                              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                                                              m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                                                              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                                                              ym(80629396, "init", {
+                                                                   clickmap:true,
+                                                                   trackLinks:true,
+                                                                   accurateTrackBounce:true,
+                                                                   webvisor:true
+                                                              });
+                                                           </script>
+                                                           <noscript><div><img src="https://mc.yandex.ru/watch/80629396" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+
+
+                                                           <script async src="https://www.googletagmanager.com/gtag/js?id=G-S8E1VB6FRK"></script>
+                                                           <script>
+                                                             window.dataLayer = window.dataLayer || [];
+                                                             function gtag(){dataLayer.push(arguments);}
+                                                             gtag('js', new Date());
+
+                                                             gtag('config', 'G-S8E1VB6FRK');
+                                                       </script>
                        </head>
                      <body>
                    <div id="app">
@@ -77,6 +117,46 @@ router.post(['/', '/everyMonth', '/everyYear', '/halfYear'], (req, res, done) =>
                              <script src='/bundle.js' defer></script>
                              <script>window.__INITIAL_DATA__= ${serialize(err)}</script>
                              <script>window.__INITIAL_TYPE__= ${serialize(typeOfSkin)}</script>
+
+                                                                 <script>
+                                                                 !function(f,b,e,v,n,t,s)
+                                                                 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                                                                 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                                                                 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                                                                 n.queue=[];t=b.createElement(e);t.async=!0;
+                                                                 t.src=v;s=b.getElementsByTagName(e)[0];
+                                                                 s.parentNode.insertBefore(t,s)}(window, document,'script',
+                                                                 'https://connect.facebook.net/en_US/fbevents.js');
+                                                                 fbq('init', '299294384104664');
+                                                                 fbq('track', 'PageView');
+                                                                 </script>
+                                                                 <noscript><img height="1" width="1" style="display:none"
+                                                                 src="https://www.facebook.com/tr?id=299294384104664&ev=PageView&noscript=1"
+                                                                 /></noscript>
+
+                                                                 <script type="text/javascript" >
+                                                                    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                                                                    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                                                                    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                                                                    ym(80629396, "init", {
+                                                                         clickmap:true,
+                                                                         trackLinks:true,
+                                                                         accurateTrackBounce:true,
+                                                                         webvisor:true
+                                                                    });
+                                                                 </script>
+                                                                 <noscript><div><img src="https://mc.yandex.ru/watch/80629396" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+
+
+                                                                 <script async src="https://www.googletagmanager.com/gtag/js?id=G-S8E1VB6FRK"></script>
+                                                                 <script>
+                                                                   window.dataLayer = window.dataLayer || [];
+                                                                   function gtag(){dataLayer.push(arguments);}
+                                                                   gtag('js', new Date());
+
+                                                                   gtag('config', 'G-S8E1VB6FRK');
+                                                                 </script>
                              </head>
                            <body>
                          <div id="app">
@@ -109,6 +189,46 @@ router.post(['/', '/everyMonth', '/everyYear', '/halfYear'], (req, res, done) =>
                    <script src='/bundle.js' defer></script>
                      <script>window.__INITIAL_ERRORS__= ${serialize(errors)}</script>
                      <script>window.__INITIAL_COND__= ${serialize(cond)}</script>
+
+                                                         <script>
+                                                         !function(f,b,e,v,n,t,s)
+                                                         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                                                         n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                                                         if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                                                         n.queue=[];t=b.createElement(e);t.async=!0;
+                                                         t.src=v;s=b.getElementsByTagName(e)[0];
+                                                         s.parentNode.insertBefore(t,s)}(window, document,'script',
+                                                         'https://connect.facebook.net/en_US/fbevents.js');
+                                                         fbq('init', '299294384104664');
+                                                         fbq('track', 'PageView');
+                                                         </script>
+                                                         <noscript><img height="1" width="1" style="display:none"
+                                                         src="https://www.facebook.com/tr?id=299294384104664&ev=PageView&noscript=1"
+                                                         /></noscript>
+
+                                                         <script type="text/javascript" >
+                                                            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                                                            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                                                            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                                                            ym(80629396, "init", {
+                                                                 clickmap:true,
+                                                                 trackLinks:true,
+                                                                 accurateTrackBounce:true,
+                                                                 webvisor:true
+                                                            });
+                                                         </script>
+                                                         <noscript><div><img src="https://mc.yandex.ru/watch/80629396" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+
+
+                                                         <script async src="https://www.googletagmanager.com/gtag/js?id=G-S8E1VB6FRK"></script>
+                                                         <script>
+                                                           window.dataLayer = window.dataLayer || [];
+                                                           function gtag(){dataLayer.push(arguments);}
+                                                           gtag('js', new Date());
+
+                                                           gtag('config', 'G-S8E1VB6FRK');
+                                                         </script>
                      <title>Практикуй английский</title>
                       </head>
                     <body>
@@ -159,6 +279,46 @@ const they = renderToString(
                          <script src='/bundle.js' defer></script>
                             <script>window.__INITIAL_STATE__ = ${serialize(indicate)}</script>
                               <script>window.__INITIAL_COND__= ${serialize(cond)}</script>
+
+                                                                  <script>
+                                                                  !function(f,b,e,v,n,t,s)
+                                                                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                                                                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                                                                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                                                                  n.queue=[];t=b.createElement(e);t.async=!0;
+                                                                  t.src=v;s=b.getElementsByTagName(e)[0];
+                                                                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                                                                  'https://connect.facebook.net/en_US/fbevents.js');
+                                                                  fbq('init', '299294384104664');
+                                                                  fbq('track', 'PageView');
+                                                                  </script>
+                                                                  <noscript><img height="1" width="1" style="display:none"
+                                                                  src="https://www.facebook.com/tr?id=299294384104664&ev=PageView&noscript=1"
+                                                                  /></noscript>
+
+                                                                  <script type="text/javascript" >
+                                                                     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                                                                     m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                                                                     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                                                                     ym(80629396, "init", {
+                                                                          clickmap:true,
+                                                                          trackLinks:true,
+                                                                          accurateTrackBounce:true,
+                                                                          webvisor:true
+                                                                     });
+                                                                  </script>
+                                                                  <noscript><div><img src="https://mc.yandex.ru/watch/80629396" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+
+
+                                                                  <script async src="https://www.googletagmanager.com/gtag/js?id=G-S8E1VB6FRK"></script>
+                                                                  <script>
+                                                                    window.dataLayer = window.dataLayer || [];
+                                                                    function gtag(){dataLayer.push(arguments);}
+                                                                    gtag('js', new Date());
+
+                                                                    gtag('config', 'G-S8E1VB6FRK');
+                                                                  </script>
                               </head>
                             <body>
                            <div id="app">
