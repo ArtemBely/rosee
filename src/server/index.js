9f5a30b96484a153e96b95155a46211e6a0d5ae6
Артем Белышев <artembelysev@MacBook-Pro-Artem.local>
2021-06-30 21:36:51 +0200

    created port

--- src/server/index.js
+++ src/server/index.js
@@ -174,4 +174,4 @@ app.use((req, res, next) => {  //<-- заменить если появится 
      next (err);
 });*/
 
-app.listen(443, () => {console.log('connected!')});
+app.listen(2053, () => {console.log('connected!')});
