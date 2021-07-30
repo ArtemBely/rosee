974c2bd5f01c877eac42d2f911860a25dfd66e26
Артем Белышев <artembelysev@MacBook-Pro-Artem.local>
2021-07-30 19:06:04 +0200

    cr11

--- src/server/index.js
+++ src/server/index.js
@@ -212,6 +212,6 @@ app.use((req, res, next) => {  //<-- заменить если появится 
 });
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
-httpServer.listen(8080, () => {console.log('connecteed!')});
-httpsServer.listen(443, () => {console.log('connecteed!')});
-//app.listen(port, () => {console.log('done')});
+//httpServer.listen(8080, () => {console.log('connecteed!')});
+//httpsServer.listen(443, () => {console.log('connecteed!')});
+app.listen(port, () => {console.log('done')});
